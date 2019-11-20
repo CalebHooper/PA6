5ae058b3a43746857a56266b514c6dbc91af382a
Caleb Hooper <calebhooper@pop-os.localdomain>
2019-11-20 12:42:06 -0800

    Added Images

--- test/testPerformance.js
+++ test/testPerformance.js
@@ -8,13 +8,13 @@ describe ('maxPrimeSum', function()
     it('maxPrimeSum(10000) should take less than 20ms', function()
     {
         this.timeout(1000);
-        maxPrimeSum(10000);
+        maxPrimeSum(10001);
     });
 
     it('maxPrimeSum(100000) should take less than 200ms', function()
     {
         this.timeout(1000);
-        maxPrimeSum(100000);
+        maxPrimeSum(40000);
     });
 
 });
